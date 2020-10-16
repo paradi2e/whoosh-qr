fae5cd82940377748dc8a030079e10d58cd72587
artemkmv <artemkayumov@easywin.us>
2020-10-16 16:09:07 +0300

    Init commit

---
+++ Whoosh QR Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whoosh QR Scanner
+//
+//  Created by Artem Kayumov on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
