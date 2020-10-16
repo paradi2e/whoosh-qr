1896636489b37b3522aac80b309576aa42830fa7
artemkmv <artemkayumov@easywin.us>
2020-10-16 18:35:20 +0300

    Add network access checker

--- Whoosh QR Scanner/ScannerViewController.swift
+++ Whoosh QR Scanner/ScannerViewController.swift
@@ -9,13 +9,13 @@ import AVFoundation
 import UIKit
 
 class ScannerViewController: UIViewController {
-
+    
     // MARK: - Public properties
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .portrait
     }
@@ -24,71 +24,84 @@ class ScannerViewController: UIViewController {
     
     private var captureSession: AVCaptureSession!
     private var previewLayer: AVCaptureVideoPreviewLayer!
-
+    
     // MARK: - Lifecycle ViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = UIColor.black
+        checkInternet()
         configureSession()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         if (captureSession?.isRunning == false) {
             captureSession.startRunning()
         }
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if (captureSession?.isRunning == true) {
             captureSession.stopRunning()
         }
     }
-
+    
     // MARK: - Private methods
+    
+    private func checkInternet() {
+        if (Reachability.isConnectedToNetwork() != true) {
+            let ac = UIAlertController(
+                title: "Нет доступа к Интернету",
+                message: "К сожалению, приложение работает только при наличии доступа к сети Интернет, подключитесь к сети и повторите попытку",
+                preferredStyle: .alert
+            )
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
     
     private func configureSession() {
         captureSession = AVCaptureSession()
-
+        
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
         
         let videoInput: AVCaptureDeviceInput
-
+        
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
             return
         }
-
+        
         if (captureSession.canAddInput(videoInput)) {
             captureSession.addInput(videoInput)
         } else {
             failed()
             return
         }
-
+        
         let metadataOutput = AVCaptureMetadataOutput()
-
+        
         if (captureSession.canAddOutput(metadataOutput)) {
             captureSession.addOutput(metadataOutput)
-
+            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.qr]
         } else {
             failed()
             return
         }
-
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = view.layer.bounds
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
-
+        
         captureSession.startRunning()
     }
     
@@ -124,12 +137,12 @@ class ScannerViewController: UIViewController {
     }
 }
 
-    // MARK: - AVCaptureMetadataOutputObjectsDelegate
+// MARK: - AVCaptureMetadataOutputObjectsDelegate
 
 extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
